7ce1cb17c31eacbb1610f4996d0c8816373a0cd1
Nitish <rananitish37@gmail.com>
2024-01-12 11:39:07 +0530

    path module methods

--- PathMod/index.js
+++ PathMod/index.js
@@ -8,3 +8,8 @@ console.log(base);
 
 const ext=path.extname("C:\programming\Node js\Node(TT)\PathMod>index.js");
 console.log(ext);
+
+const detail=path.parse("C:\programming\Node js\Node(TT)\PathMod>index.js");
+console.log(detail);
+
+console.log(detail.name);
