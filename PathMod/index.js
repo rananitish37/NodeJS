10a6d3c1eba21f7a2c3b6bcc3140297c754b1b59
Nitish <rananitish37@gmail.com>
2024-01-12 11:37:08 +0530

    path module

---
+++ PathMod/index.js
@@ -0,0 +1,10 @@
+const path=require("path");
+
+const dir=path.dirname("C:\programming\Node js\Node(TT)\PathMod>index.js");
+console.log(dir);
+
+const base=path.basename("C:\programming\Node js\Node(TT)\PathMod>index.js");
+console.log(base);
+
+const ext=path.extname("C:\programming\Node js\Node(TT)\PathMod>index.js");
+console.log(ext);
